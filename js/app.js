2f1f77cba1f04b1936e2b4309884cd02f9bf2375
Assiya Kh <assiyakhuzyakhmetova@gmail.com>
2018-01-23 19:16:06 +0600

    24th January commit

---
+++ js/app.js
@@ -0,0 +1,221 @@
+// global variables to prevent multiple calls to API
+var map, infoWindow;
+// Model part with locations data list
+// Those are universities of Astana
+var locations = data;
+
+function initMap() {
+    // responsive for size of browser window
+    const mq = window.matchMedia("(max-width: 413px)");
+    var zoom_var;
+
+    if (mq.matches) {
+        zoom_var = 11;
+    } else {
+        zoom_var = 17;
+    }
+    // initialize map centered at Astana
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 51.1605,
+            lng: 71.4704
+        },
+        zoom: zoom_var
+    });
+}
+
+// Error Handling from Google Maps
+function error() {
+        alert("The error was occured. Try to reload the page later");
+    }
+    // Constractor for University that takes data from model as argument
+var University = function(item) {
+    this.title = item.title;
+    this.located = item.located;
+    this.logo = item.logo;
+    this.marker = null;
+};
+
+// Creates a marker with color property
+function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+        'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+        '|40|_|%E2%80%A2',
+        new google.maps.Size(28, 43),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(10, 34),
+        new google.maps.Size(28, 43));
+    return markerImage;
+}
+
+// VM part of KnockoutJS MVVM
+var ViewModel = function() {
+
+    var self = this;
+
+    document.getElementById('hide-listings').addEventListener('click', hideListings);
+    document.getElementById('show-listings').addEventListener('click', showListings);
+    document.getElementById('clear').addEventListener('click', clearForm);
+
+    // highlights an icon when mouse is over it
+    var defaultIcon = makeMarkerIcon('FB6733');
+    var highlightedIcon = makeMarkerIcon('FFFF24');
+
+    infoWindow = new google.maps.InfoWindow();
+    self.locations_list = [];
+    // creates Location objects with data from Model
+    locations.forEach(function(item) {
+        self.locations_list.push(new University(item));
+    });
+
+    var bounds = new google.maps.LatLngBounds();
+    // markers on map
+    self.locations_list.forEach(function(item) {
+        item.marker = new google.maps.Marker({
+            map: map,
+            position: item.located,
+            logo: item.logo,
+            icon: defaultIcon
+        });
+
+        item.marker.setMap(map);
+        bounds.extend(item.marker.position);
+        map.fitBounds(bounds);
+
+        //adds the 'click' event to marker
+        google.maps.event.addListener(item.marker, 'click', function() {
+            item.marker.setAnimation(google.maps.Animation.DROP);
+            infoWindow.open(map, item.marker);
+            infoWindow.setContent(item.title + '<br>' + '<p>Latitude: ' + item.located.lat +
+                '<br>' + '<p>Longitude: ' + item.located.lng +
+                '<br> <p style="text-align:center"><img src="' +
+                item.logo + '" width="100"></p><br><p>Wikipedia articles about this place: </p>' + wikiRes);
+        });
+
+        //adds the 'mouseoover' & 'mouseout' event to marker
+        google.maps.event.addListener(item.marker, 'mouseover', function() {
+            item.marker.setIcon(highlightedIcon);
+
+
+        });
+        google.maps.event.addListener(item.marker, 'mouseout', function() {
+            item.marker.setIcon(defaultIcon);
+        });
+
+        // request to Wikipedia API to find articles about universities
+        var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' +
+            item.title + '&format=json&callback=wikiCallback';
+
+
+        // There could be several result
+        var wikiRes = "";
+        // Not to wait too long if server is not responding
+        var wikiRequestTimeout = setTimeout(function() {
+            wikiRes = "Cannot load articles for " + item.title + ". Try to refresh page";
+        }, 2000);
+
+        //AJAX request to Wikipedia
+        $.ajax({
+
+            url: wikiUrl,
+            dataType: "jsonp",
+
+            success: function(response) {
+                var articleList = response[1];
+                for (var i = 0; i < articleList.length; i++) {
+                    articleStr = articleList[i];
+                    var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                    wikiRes += '<li><a href="' + url + '" target="_blank">' +
+                        articleStr + '</a></li>'
+                };
+                clearTimeout(wikiRequestTimeout);
+            }
+        });
+    });
+
+    // Shows clicked item from the list on the map
+    self.clicked = function(clicked_item) {
+        self.locations_list.forEach(function(item) {
+            if (clicked_item.title != item.title) {
+                item.marker.setVisible(false);
+            } else {
+                item.marker.setVisible(true);
+                item.marker.setMap(map);
+                bounds.extend(item.marker.position);
+                infoWindow.open(map, item.marker);
+                infoWindow.setContent(item.title + '<p> Click on the marker to find out more </p>');
+            }
+        });
+    }
+
+
+    // Hides markers when button 'Hide' is pressed
+    // They are shown initially by default
+    function hideListings() {
+        self.locations_list.forEach(function(item) {
+            item.marker.setMap(null);
+        });
+    }
+
+    // Displays markers on the map when 'Show' is pressed
+    function showListings() {
+        var bounds = new google.maps.LatLngBounds();
+        self.locations_list.forEach(function(item) {
+			infoWindow.close();
+            item.marker.setVisible(true);
+            item.marker.setMap(map);
+            bounds.extend(item.marker.position);
+        });
+        map.fitBounds(bounds);
+    }
+
+    // searches for universities by its title
+    self.search_result = ko.observableArray();
+    self.searchInput = ko.observable('');
+
+    // button that cleares entered text
+    function clearForm() {
+            searchInput('');
+        }
+        // adds all locations to the list to display
+    self.locations_list.forEach(function(item) {
+        self.search_result.push(item);
+    });
+
+
+    // search function that sets only marker of satisfying element to visible
+    // searches occurence of chars from input in the title of item
+    self.search = function() {
+        // cleans the list 
+        hideListings();
+        // takes the value from observable
+        var searchItem = self.searchInput();
+        // clears the previous search result 
+        self.search_result.removeAll();
+        // adds if a match
+        self.locations_list.forEach(function(item) {
+            if (item.title.indexOf(searchItem) !== -1) {
+                self.search_result.push(item);
+                item.marker.setVisible(true);
+                // extends bounds to fit found markers
+                item.marker.setMap(map);
+                bounds.extend(item.marker.position);
+                // calls the infoWindow
+                infoWindow.open(map, item.marker);
+                infoWindow.setContent(item.title + '<p> Click on the marker to find out more </p>')
+            }
+        });
+    };
+};
+
+// Google Maps API Server returns a callback to initApp
+// as an answer to the request 
+
+function initApp() {
+	//initMap is called to initialize a map
+    initMap();
+	// binding is applied
+    viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+
+};
---
+++ js/data.js
@@ -0,0 +1,43 @@
+var data = [{
+    title: 'L.N.Gumilyov Eurasian National University',
+    logo: 'http://www.enu.kz/images/novosti-enu/enu-logo-new.png',
+    located: {
+        lat: 51.16031865222501,
+        lng: 71.46481990814209
+    }
+}, {
+    title: 'Kazakh University of Economics, Finance and International Trade',
+    logo: 'https://www.spr.kz/pages_logotip/2014-06/18029.jpg',
+    located: {
+        lat: 51.159968767920475,
+        lng: 71.45771741867065
+    }
+}, {
+    title: 'QSI International School of Astana',
+    logo: 'https://www.qsi.org/images/logo.png',
+    located: {
+        lat: 51.146094,
+        lng: 71.389589
+    }
+}, {
+    title: 'Nazarbayev University',
+    logo: 'https://nu.edu.kz/wp-content/themes/influence-child/images/logo.png',
+    located: {
+        lat: 51.0905854777853,
+        lng: 71.39808654785156
+    }
+}, {
+    title: 'Haileybury Astana',
+    logo: 'https://upload.wikimedia.org/wikipedia/en/0/0e/Haileybury_Astana_Logo.png',
+    located: {
+        lat: 51.110036,
+        lng: 71.457261
+    }
+}, {
+    title: 'Miras International School Astana',
+    logo: 'http://www.meibaschools.org/MeibaSchoolImage/MirMIRASBannerjpg634888874620392196.jpg ',
+    located: {
+        lat: 51.151520,
+        lng: 71.487628
+    }
+}];
